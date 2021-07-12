27a5261146dd9ef7ca84ca99fc99c2a89ee31c11
Valentina-Fr <v_frasca@virgilio.it>
2021-07-12 15:33:18 +0200

    Add function

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ Creare una funzione per capire se la parola inserita è palindroma
 */
 
 //Chiedere all'utente una parola
-var userWord = prompt("Inserisci una parola");
+var userWord = isPalindrome(prompt("Inserisci una parola"));
+document.getElementById("palindrome").innerText = userWord;
+
+//Creare funzione
+function isPalindrome(word) {
+    var backward = word.split("").reverse().join("");
+    var result = "La parola non è palindroma";
+    if (word === backward) {
+        result = "La parola è palindroma";
+    } 
+    return result;
+}
